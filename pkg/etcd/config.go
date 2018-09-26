1986cb0027b058d359e153d604c8d35e1efb6e2c
Brian Kelley <brian.kelley@jamf.com>
2018-09-26 15:43:51 -0500

    Remove anything related to TLS config

--- pkg/etcd/config.go
+++ pkg/etcd/config.go
@@ -15,15 +15,9 @@ func env(name, defaults string) string {
 func GetEnvConfig() Config {
 	return Config{
 		EnvFile:        env("ETCD_ENV_FILE", "/etc/etcd/config"),
-		ClientScheme:   env("ETCD_CLIENT_SCHEME", "https"),
+		ClientScheme:   env("ETCD_CLIENT_SCHEME", "http"),
 		ClientPort:     env("ETCD_CLIENT_PORT", "2379"),
-		ClientCAFile:   env("ETCD_CLIENT_CA_FILE", "/etc/etcd/certs/ca.pem"),
-		ClientCertFile: env("ETCD_CLIENT_CERT_FILE", "/etc/etcd/certs/etcd.pem"),
-		ClientKeyFile:  env("ETCD_CLIENT_KEY_FILE", "/etc/etcd/certs/etcd-key.pem"),
-		PeerScheme:     env("ETCD_PEER_SCHEME", "https"),
+		PeerScheme:     env("ETCD_PEER_SCHEME", "http"),
 		PeerPort:       env("ETCD_PEER_PORT", "2380"),
-		PeerCAFile:     env("ETCD_PEER_CA_FILE", "/etc/etcd/certs/peer-ca.pem"),
-		PeerCertFile:   env("ETCD_PEER_CERT_FILE", "/etc/etcd/certs/peer-etcd.pem"),
-		PeerKeyFile:    env("ETCD_PEER_KEY_FILE", "/etc/etcd/certs/peer-etcd-key.pem"),
 	}
 }
